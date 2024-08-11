b086cced08d4e2b7f2d6b23819c4386f0b95a7ea
lazbord <lazarebordereaux@yahoo.fr>
2024-08-11 16:21:50 +0200

    added basic password hashing

--- services/auth/database/mongo.go
+++ services/auth/database/mongo.go
@@ -65,8 +65,3 @@ func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 	return string(bytes), err
 }
-
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
-}
