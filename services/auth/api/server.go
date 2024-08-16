0afe55a629428e2df31f65cb2cb94e5070f56017
lazbord <lazarebordereaux@yahoo.fr>
2024-08-16 14:52:40 +0200

    beginning of streaming service

--- services/auth/api/server.go
+++ services/auth/api/server.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/lazbord/SpotyGo/services/auth/service"
 )
@@ -17,6 +20,16 @@ func NewApiAdapter(authService *service.AuthService) *ApiAdapter {
 
 func (api ApiAdapter) NewAPI() {
 	router := gin.Default()
+
+	// Configure CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8081"}, // React Native dev server origin
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	router.POST("/login", api.Login)
 	router.POST("/create/user", api.Test)
