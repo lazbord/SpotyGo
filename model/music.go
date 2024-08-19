4b4ae532077404793c5d522b55b122c4927a720e
lazbord <lazarebordereaux@yahoo.fr>
2024-08-19 12:02:02 +0200

    dk why music.go isnt pushed

--- model/music.go
+++ model/music.go
@@ -1,10 +1,12 @@
 package model
 
 type Music struct {
-	ID       string `bson:"_id,omitempty" json:"id,omitempty"`
-	Name     string `bson:"name" json:"name"`
-	Artist   string `bson:"artist" json:"artist"`
-	Duration string `bson:"duration" json:"duration"`
-	Data     []byte `bson:"data" json:"data"`
-	Filename string `bson:"filename" json:"filename"`
+	ID        string `bson:"_id,omitempty" json:"id,omitempty"`
+	VideoId   string `bson:"videoid" json:"video"`
+	Name      string `bson:"name" json:"name"`
+	Artist    string `bson:"artist" json:"artist"`
+	Thumbnail string `bson:"thumbnail" json:"thumbnail"`
+	Duration  string `bson:"duration" json:"duration"`
+	Data      []byte `bson:"data" json:"data"`
+	Filename  string `bson:"filename" json:"filename"`
 }
